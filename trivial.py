c4cd45bf91cbb207b16afdad100c069e8c61ecfe
Nick Graham <nickbgraham83@gmail.com>
2017-11-19 10:30:07 -0600

    Adds a trivial model (and results from "one tree" many)

--- trivial.py
+++ trivial.py
@@ -15,16 +15,17 @@ def main():
 
     best_preds = [[0,1] if pred > 0.5 else [1,0] for t in test_target]
     auc, acc = test_f_stars(best_preds, test_target, f_stars)
-
     print("Always predict: ", "No" if pred < 0.5 else "Yes")
     print("  AUC: ", auc, " Accuracy: ", acc)
 
     rand_preds = [[0,1] if np.random.rand() < pred else [1,0] for t in test_target]
     auc, acc = test_f_stars(rand_preds, test_target, f_stars)
-
-    print("Predict yes with prob: ", pred)
+    print("Predict yes {}% of the time".format(pred))
     print("  AUC: ", auc, " Accuracy: ", acc)
 
+    auc, acc = test_f_stars([[1-pred, pred]]*len(test_target), test_target, f_stars)
+    print("Always predict event prob as {}%".format(pred))
+    print("  AUC: ", auc, " Accuracy: ", acc)
 
 
 if __name__ == "__main__":
