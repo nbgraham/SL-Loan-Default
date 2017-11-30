b2bfffb469d29ce40a26ac612cc75e67f0260a5b
Nick Graham <nickbgraham83@gmail.com>
2017-11-30 13:49:35 -0600

    Checkpoints in dec tree

--- dec_tree_experiment.py
+++ dec_tree_experiment.py
@@ -3,7 +3,7 @@ import numpy as np
 
 from decision_tree import DecisionTreeClassifier
 from data import load_loan, load_loan_no_history
-from model_experiment import test_model
+from model_experiment import _test_model
 
 
 def main(history=True):
@@ -18,13 +18,17 @@ def test_dec_tree(data, target, attributes):
     _max_depths = [i for i in range(5,11)]
     fs = ['gini', 'entropy']
 
-    experiment_data = test_model(data, target, attributes, create_decision_tree, fs, _max_depths, _min_samples)
+    experiment_data = _test_model(data, target, attributes, create_decision_tree, save, fs, _max_depths, _min_samples)
 
+    save(experiment_data['auc_grid'], experiment_data['acc_grid'])
+
+
+def save(auc_grid, acc_grid):
     with open('dec_tree_auc.npy', 'wb') as auc:
-        np.save(auc, experiment_data['auc_grid'])
+        np.save(auc, auc_grid)
 
     with open('dec_tree_acc.npy', 'wb') as acc:
-        np.save(acc, experiment_data['acc_grid'])
+        np.save(acc, acc_grid)
 
 
 def create_decision_tree(f, max_depth, min_sm):
