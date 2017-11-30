1061112c6e33b7b6fa970afbea388e6d7293b222
Nick Graham <nickbgraham83@gmail.com>
2017-11-29 20:00:52 -0600

    Adds adaboost progress bar

--- adaboost.py
+++ adaboost.py
@@ -1,22 +1,25 @@
 import numpy as np
 from math import log2
+import progressbar
 
 from decision_tree import DecisionTreeClassifier
 
 
-def default_create_model(min_split_size=100, max_depth=4, remainder_score='entropy'):
+def default_create_model(min_split_size=None, max_depth=4, remainder_score='entropy'):
     return DecisionTreeClassifier(min_split_size=min_split_size, max_depth=max_depth,
                        remainder_score=remainder_score, show_progress=False)
 
 
 class Adaboost:
-    def __init__(self,max_depth=4, n_models=10, learning_rate = 1, create_model=default_create_model):
+    def __init__(self,max_depth=4, n_models=10, learning_rate = 1, create_model=default_create_model, show_progress=True):
         self.n_models = n_models
         self.max_depth = max_depth
         self.learning_rate = learning_rate
         self.create_model = create_model
         self.models = []
         self.errs = []
+        self.show_progress = show_progress
+        self.progress_bar = None
 
     def fit(self, x, y, attributes):
         self.models = []
@@ -24,7 +27,16 @@ class Adaboost:
 
         weights = np.ones(len(x))/len(x)
 
+        if self.show_progress:
+            self.progress_bar = progressbar.ProgressBar(max_value=self.n_models, widgets=[
+                'Building Adaboost classifier ', progressbar.Percentage(), ' (', progressbar.SimpleProgress(), ') ',
+                progressbar.Bar(),
+                progressbar.Timer(), ' ', progressbar.ETA()
+            ])
+
         for i in range(self.n_models):
+            if self.show_progress:
+                self.progress_bar.update(i)
             model = self.create_model(max_depth=self.max_depth)
             model.fit(x, y, attributes, weights=weights)
             predictions = model.predict(x)
