8630ba92f331c0b4aa5be11751d78048368271fd
Nick Graham <nickbgraham83@gmail.com>
2017-11-30 18:12:43 -0600

    Reliability based scaling

--- random_forest_test.py
+++ random_forest_test.py
@@ -31,5 +31,42 @@ def main():
     # print("AUC: ", auc, " Accuracy: ", acc)
 
 
+def best_rel():
+    data, target, attributes = load_loan()
+    training_val_data, training_val_target, test_data, test_target = split(data, target)
+    training_data, training_target, val_data, val_target = split(training_val_data, training_val_target)
+
+    clf = RandomForestClassifier(max_depth=5, n_trees=70, show_progress=True)
+    clf.fit(training_data, training_target, attributes)
+
+    test_pred = clf.predict_prob(test_data)
+    orig_test_rel = rel(test_pred[:, 1], test_target)
+
+    val_pred = clf.predict_prob(val_data) # w/100 trees AUC = 0.78
+    min_rel = 1
+    best_b = 0
+    best_m = 1
+    for b in range(0,10):
+        b = 0.05 - b/100
+        for m in range(0, 11):
+            m= 1 + (m-5)/30
+            cur_rel = rel(m*val_pred[:, 1]+b, val_target)
+            print("m: {} b: {} rel: {}".format(m,b,cur_rel))
+            if cur_rel < min_rel:
+                min_rel = cur_rel
+                best_b = b
+                best_m = m
+    print("\nBest m: {} b: {} with rel: {}".format(best_m, best_b, min_rel))
+
+    test_pred = clf.predict_prob(test_data)
+    test_pred = best_m * test_pred + best_b
+    test_rel = rel(test_pred[:,1], test_target)
+    print("Original Test Rel: ", orig_test_rel, "Test Rel: ", test_rel)
+
+    f_stars = [i/100 for i in range(100)]
+    auc, acc, pod, pofd = test_f_stars(test_pred, test_target, f_stars)
+    print("AUC: ", auc, " Accuracy: ", acc," POD: ", pod, " POFD: ", pofd)
+
+
 if __name__ == "__main__":
-    main()
+    best_rel()
