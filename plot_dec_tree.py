bfbfe83d0852dbc909e8aa131c66e5e39773233d
Nick Graham <nickbgraham83@gmail.com>
2017-12-01 15:49:15 -0600

    Option to plot AUC and Rel with or without history

--- plot_dec_tree.py
+++ plot_dec_tree.py
@@ -1,9 +1,12 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
+HISTORY = True
 
-def plot():
-    with open('dec_tree_auc.npy', 'rb') as f:
+
+def plot_auc():
+    suffix = '' if HISTORY else '_no_history'
+    with open('dec_tree_auc' + suffix + '.npy', 'rb') as f:
         a = np.load(f)
 
     gini = a [0]
@@ -19,7 +22,7 @@ def plot():
 
     plt.imshow(gini)
     plt.colorbar()
-    plt.title("Decision Tree Gini: AUC")
+    plt.title("Decision Tree No History Gini: AUC")
     plt.ylabel("Max depth")
     plt.yticks(range(len(max_depths)), max_depths)
     plt.xlabel("Min split size")
@@ -28,7 +31,43 @@ def plot():
 
     plt.imshow(entropy)
     plt.colorbar()
-    plt.title("Decision Tree Entropy: AUC")
+    plt.title("Decision Tree No History Entropy: AUC")
+    plt.ylabel("Max depth")
+    plt.yticks(range(len(max_depths)), max_depths)
+    plt.xlabel("Min split size")
+    plt.xticks(range(len(min_split_sizes)), min_split_sizes)
+    plt.show()
+
+
+def plot_rel():
+    suffix = '' if HISTORY else '_no_history'
+    with open('dec_tree_auc' + suffix + '.npy', 'rb') as f:
+        a = np.load(f)
+
+    gini = a [0]
+    entropy = a [1]
+
+    scoring = ['gini', 'entropy']
+    min_split_sizes = [j for j in range(50, 150, 20)]
+    max_depths = [i for i in range(5, 11)]
+
+    max_auc = a.min()
+    max_run = np.unravel_index(a.argmin(), a.shape)
+    print("Min reliability: {} with max depth: {} min split size: {} and scoring: {}".format(max_auc, max_depths[max_run[1]], min_split_sizes[max_run[2]], scoring[max_run[0]]))
+
+    qual = '' if HISTORY else ' No History'
+    plt.imshow(gini)
+    plt.colorbar()
+    plt.title("Decision Tree" + qual + " Gini: Reliability")
+    plt.ylabel("Max depth")
+    plt.yticks(range(len(max_depths)), max_depths)
+    plt.xlabel("Min split size")
+    plt.xticks(range(len(min_split_sizes)), min_split_sizes)
+    plt.show()
+
+    plt.imshow(entropy)
+    plt.colorbar()
+    plt.title("Decision Tree" + qual + " Entropy: Reliability")
     plt.ylabel("Max depth")
     plt.yticks(range(len(max_depths)), max_depths)
     plt.xlabel("Min split size")
@@ -37,4 +76,4 @@ def plot():
 
 
 if __name__ == "__main__":
-    plot()
+    plot_rel()
