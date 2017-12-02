6a8c1fe527c7b21f5e2d7f40b7186a2fe90c7c10
Nick Graham <nickbgraham83@gmail.com>
2017-12-02 10:24:58 -0600

    Adds result for random forest

--- results.py
+++ results.py
@@ -1,8 +1,9 @@
 from sklearn.tree import DecisionTreeClassifier as SKLearnDecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier as SKLearnRandomForestClassifier
 
 from analysis import test_f_stars, rel, inv_rel
 from data import load_loan, split
-from random_forest import RandomForestClassifier
+from random_forest import RandomForestClassifier as MyRandomForestClassifier
 from decision_tree import DecisionTreeClassifier as MyDecisionTreeClassifier
 from wrapper import Wrapper
 
@@ -11,8 +12,8 @@ def main():
     data, target, attributes = load_loan()
     training_val_data, training_val_target, test_data, test_target = split(data, target)
 
-    clf = Wrapper(my_code=False, my_model=MyDecisionTreeClassifier(max_depth=7, min_split_size=110, remainder_score='entropy'), sklearn_model=SKLearnDecisionTreeClassifier(criterion='entropy', max_depth=7, min_samples_split=110))
-
+    # clf = Wrapper(my_code=False, my_model=MyDecisionTreeClassifier(max_depth=7, min_split_size=110, remainder_score='entropy'), sklearn_model=SKLearnDecisionTreeClassifier(criterion='entropy', max_depth=7, min_samples_split=110))
+    clf = Wrapper(my_code=False, my_model=MyRandomForestClassifier(max_depth=5, n_trees=70, remainder_score='gini'), sklearn_model=SKLearnRandomForestClassifier(criterion='gini', max_depth=5, n_estimators=70))
     clf.fit(training_val_data, training_val_target, attributes)
 
     pred = clf.predict_prob(test_data) # w/100 trees AUC = 0.78
